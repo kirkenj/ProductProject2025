47c3219d090b3735805cb6f7f5a0ae52756a3f09
kirkenj <kirkenj@bk.ru>
2025-03-23 20:51:56 +0300

    Transformed openApi document aggregation into json-like way

--- Gateway/Gateway/Middlewares/OpenApiMiddleware.cs
+++ Gateway/Gateway/Middlewares/OpenApiMiddleware.cs
@@ -1,7 +1,6 @@
-﻿using System.Text;
+﻿using System.Text.Json.Nodes;
 using Gateway.Models;
 using Microsoft.Extensions.Options;
-using Microsoft.OpenApi.Models;
 using Microsoft.OpenApi.Readers;
 using Microsoft.OpenApi.Writers;
 
@@ -9,6 +8,10 @@ namespace Gateway.Middlewares
 {
     public class OpenApiMiddleware : IMiddleware
     {
+        private const string COMPONENTS_TAG = "components";
+        private const string SCHEMAS_TAG = "schemas";
+        private const string PATH_TAG = "paths";
+
         private readonly ServicesSettings _options;
         private readonly ILogger<OpenApiMiddleware> _logger;
         private readonly HttpClient _httpClient;
@@ -27,71 +30,13 @@ namespace Gateway.Middlewares
             _options = options.Value;
         }
 
-
         public async Task InvokeAsync(HttpContext context, RequestDelegate next)
         {
-            if (context.Request.Method == "GET" 
-                && context.Request.Path.HasValue 
+            if (context.Request.Method == "GET"
+                && context.Request.Path.HasValue
                 && context.Request.Path.Value == _options.OpenApiPathPrefixSegment)
             {
-                var openApiDocuments = await Task.WhenAll(_options.ServiceConfigs.Select(GetDocumentationForService));
-
-                var nullStreamsCount = openApiDocuments.Count(c => c == null);
-                if (nullStreamsCount > 0)
-                {
-                    _logger.LogWarning("Couldn't recieve openApi documents from {invalidRequestsCount} services", nullStreamsCount);
-                }
-                else
-                {
-                    _logger.LogInformation("All requests for openApi documents succeded");
-                }
-
-                var documentStreamsToHandle = openApiDocuments.Where(c => c != null);
-
-                OpenApiStreamReader openApiReader = new();
-                
-                OpenApiDocument? resultDocument = null;
-
-                foreach (var openApiDocument in documentStreamsToHandle)
-                { 
-                    OpenApiDocument openApiDoc = openApiReader.Read(openApiDocument, out var diagnostic);
-                    if (diagnostic.Errors.Count > 0)
-                    {
-                        var concatedErrors = string.Join("\r\n", diagnostic.Errors.Select(e => e.Message));
-                        _logger.LogError("Errors found in the OpenAPI document: {errors}", concatedErrors);
-                        continue;
-                    }
-
-                    if (resultDocument == null)
-                    {
-                        resultDocument = openApiDoc;
-                        continue;
-                    }
-
-                    CombineOpenApiDocuments(resultDocument, openApiDoc);
-                }
-
-                if (resultDocument == null)
-                {
-                    var message = "Couldn't aggregate openApi docs";
-                    _logger.LogError(message);
-                    WriteTextResponse(context, message, 500);
-                    return;
-                }
-
-                resultDocument.Info.Title = _options.GatewayOpenApiDocumentName;
-
-                using var stringWriter = new StringWriter();
-                var openApiWriter = new OpenApiJsonWriter(stringWriter);
-                resultDocument.SerializeAsV2(openApiWriter);
-
-                string openApiString = stringWriter.ToString();
-
-                var bytes = Encoding.UTF8.GetBytes(openApiString);
-
-                context.Response.StatusCode = 200;
-                context.Response.ContentType = "application/json";
-                await context.Response.BodyWriter.WriteAsync(bytes);
+                await HandleGetOpenApiDocumentRequestAsync(context);
             }
             else
             {
@@ -99,7 +44,94 @@ namespace Gateway.Middlewares
             }
         }
 
-        private async Task<Stream?> GetDocumentationForService(ServiceConfig serviceConfig)
+        private async Task HandleGetOpenApiDocumentRequestAsync(HttpContext context)
+        {
+            var openApiDocuments = await Task.WhenAll(_options.ServiceConfigs.Select(GetDocumentationForService));
+
+            var nullStreamsCount = openApiDocuments.Count(c => c == null);
+            if (nullStreamsCount > 0)
+            {
+                _logger.LogWarning("Couldn't recieve openApi documents from {invalidRequestsCount} services", nullStreamsCount);
+            }
+            else
+            {
+                _logger.LogInformation("All requests for openApi documents succeded");
+            }
+
+            var documentStreamsToHandle = openApiDocuments.Where(c => c != null);
+
+            string? resultDocument = null;
+
+            foreach (var openApiDocument in documentStreamsToHandle)
+            {
+                if (resultDocument == null)
+                {
+                    resultDocument = openApiDocument;
+                    continue;
+                }
+
+                resultDocument = CombineOpenApiDocuments(resultDocument, openApiDocument!);
+            }
+
+            if (resultDocument == null)
+            {
+                var message = "Couldn't aggregate openApi docs";
+                _logger.LogError(message);
+                WriteResponse(context, message, 500);
+                return;
+            }
+
+            var reader = new OpenApiStringReader();
+
+            var openApiDoc = reader.Read(resultDocument, out var diagnostic);
+            if (diagnostic.Errors.Count > 0)
+            {
+                var concatedErrors = string.Join("\r\n", diagnostic.Errors.Select(e => e.Message));
+                _logger.LogError("Errors found in the OpenAPI document: {errors}", concatedErrors);
+            }
+
+            openApiDoc.Info.Title = _options.GatewayOpenApiDocumentName;
+
+            using var stringWriter = new StringWriter();
+            var openApiWriter = new OpenApiJsonWriter(stringWriter);
+            openApiDoc.SerializeAsV2(openApiWriter);
+
+            string openApiString = stringWriter.ToString();
+
+            WriteResponse(context, openApiString, 200, "application/json");
+        }
+
+        private static string CombineOpenApiDocuments(string firstDocument, string secondDocument)
+        {
+            var firstDocumentJson = JsonObject.Parse(firstDocument) ?? throw new Exception($"Couldn't parse jsonObject from {nameof(firstDocument)}");
+            var secondDocumentJson = JsonObject.Parse(secondDocument) ?? throw new Exception($"Couldn't parse jsonObject from {nameof(secondDocument)}");
+
+            var firstComponentsSection = firstDocumentJson[COMPONENTS_TAG]?[SCHEMAS_TAG]?.AsObject()
+                ?? throw new Exception($"Couldn't get jsonObject from {nameof(firstDocumentJson)} with key {COMPONENTS_TAG}:{SCHEMAS_TAG}");
+            var secondComponentsSection = secondDocumentJson[COMPONENTS_TAG]?[SCHEMAS_TAG]?.AsObject()
+                ?? throw new Exception($"Couldn't get jsonObject from {nameof(secondDocumentJson)} with key {COMPONENTS_TAG}:{SCHEMAS_TAG}");
+
+            foreach (var component in secondComponentsSection.ToArray())
+            {
+                secondComponentsSection.Remove(component.Key);
+                firstComponentsSection.Add(component);
+            }
+
+            var firstPathsSection = firstDocumentJson[PATH_TAG]?.AsObject()
+                ?? throw new Exception($"Couldn't get jsonObject from {nameof(firstDocumentJson)} with key {PATH_TAG}");
+            var secondPathssSection = secondDocumentJson[PATH_TAG]?.AsObject()
+                ?? throw new Exception($"Couldn't get jsonObject from {nameof(secondDocumentJson)} with key {PATH_TAG}");
+
+            foreach (var component in secondPathssSection.ToArray())
+            {
+                secondPathssSection.Remove(component.Key);
+                firstPathsSection.Add(component);
+            }
+
+            return firstDocumentJson.ToJsonString();
+        }
+
+        private async Task<string?> GetDocumentationForService(ServiceConfig serviceConfig)
         {
             try
             {
@@ -112,7 +144,7 @@ namespace Gateway.Middlewares
                     return null;
                 }
 
-                return await result.Content.ReadAsStreamAsync();
+                return await result.Content.ReadAsStringAsync();
             }
             catch (Exception ex)
             {
@@ -121,45 +153,11 @@ namespace Gateway.Middlewares
             }
         }
 
-        private static void WriteTextResponse(HttpContext context, string message, int statusCode)
+        private static void WriteResponse(HttpContext context, string message, int statusCode, string contentType = "text/html")
         {
             context.Response.StatusCode = statusCode;
+            context.Response.ContentType = contentType;
             context.Response.WriteAsync(message);
-            context.Response.ContentType = "text/html";
-        }
-
-        private static void CombineOpenApiDocuments(OpenApiDocument destinationDocument, OpenApiDocument sourceDocument) 
-        {
-
-            foreach (var path in sourceDocument.Paths)
-            {
-                destinationDocument.Paths.Add(path.Key, path.Value);
-            }
-
-            foreach (var ext in sourceDocument.Components.Extensions)
-            {
-                destinationDocument.Components.Extensions.Add(ext);
-            }
-
-            foreach (var schema in sourceDocument.Components.Examples)
-            {
-                destinationDocument.Components.Examples.Add(schema.Key, schema.Value);
-            }
-
-            foreach (var schema in sourceDocument.Components.Schemas)
-            {
-                destinationDocument.Components.Schemas.Add(schema.Key, schema.Value);
-            }
-
-            foreach (var schema in sourceDocument.Components.Responses)
-            {
-                destinationDocument.Components.Responses.Add(schema.Key, schema.Value);
-            }
-
-            foreach (var schema in sourceDocument.Components.RequestBodies)
-            {
-                destinationDocument.Components.RequestBodies.Add(schema.Key, schema.Value);
-            }
         }
     }
 }
